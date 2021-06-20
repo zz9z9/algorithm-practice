9bf505bb091062c0e6583200cd99bfa0f69a0fe9
Jaeyoon <wodbs4338@naver.com>
2021-06-20 23:11:06 +0900

    셔틀버스 문제 해결

--- README.md
+++ README.md
@@ -1,18 +1,21 @@
-## 프로그래머스
+## 프로그래머스 
+🟠 : 오래걸림 <br>
+🔴 : 답 보고 해결 
 - prob1 : [네트워크](https://github.com/zz9z9/algorithm-practice/tree/master/src/programmers/prob1)
-- prob2 : [다리를 지나는 트럭](https://github.com/zz9z9/algorithm-practice/tree/master/src/programmers/prob2) - 오래 걸림 
+- prob2 : [다리를 지나는 트럭](https://github.com/zz9z9/algorithm-practice/tree/master/src/programmers/prob2) 🟠 
 - prob3 : [신규 아이디 추천](https://github.com/zz9z9/algorithm-practice/tree/master/src/programmers/prob3)
 - prob4 : [카펫](https://github.com/zz9z9/algorithm-practice/tree/master/src/programmers/prob4)
-- prob5 : [여행경로](https://github.com/zz9z9/algorithm-practice/tree/master/src/programmers/prob5) - 오래 걸림 
+- prob5 : [여행경로](https://github.com/zz9z9/algorithm-practice/tree/master/src/programmers/prob5) 🟠 
 - prob6 : [이중우선순위큐](https://github.com/zz9z9/algorithm-practice/tree/master/src/programmers/prob6)
-- prob7 : [순위](https://github.com/zz9z9/algorithm-practice/tree/master/src/programmers/prob7) - 답 보고 해결 
+- prob7 : [순위](https://github.com/zz9z9/algorithm-practice/tree/master/src/programmers/prob7) 🔴 
 - prob8 : [키패드 누르기](https://github.com/zz9z9/algorithm-practice/tree/master/src/programmers/prob8)
 - prob9 : [수식 최대화](https://github.com/zz9z9/algorithm-practice/tree/master/src/programmers/prob9)
 - prob10 : [오픈 채팅방](https://github.com/zz9z9/algorithm-practice/tree/master/src/programmers/prob10)
 - prob11 : [크레인 인형 뽑기](https://github.com/zz9z9/algorithm-practice/tree/master/src/programmers/prob11)
 - prob12 : [점프와 순간 이동](https://github.com/zz9z9/algorithm-practice/tree/master/src/programmers/prob12)
-- prob13 : [입국심사](https://github.com/zz9z9/algorithm-practice/tree/master/src/programmers/prob13) - 답 보고 해결 
-- prob14 : [행렬의 곱셈](https://github.com/zz9z9/algorithm-practice/tree/master/src/programmers/prob14) - 오래 걸림 
+- prob13 : [입국심사](https://github.com/zz9z9/algorithm-practice/tree/master/src/programmers/prob13) 🔴  
+- prob14 : [행렬의 곱셈](https://github.com/zz9z9/algorithm-practice/tree/master/src/programmers/prob14) 🟠
+- prob15 : [[1차] 셔틀버스](https://github.com/zz9z9/algorithm-practice/tree/master/src/programmers/prob15) 🟠  
 
 ## 백준
 - prob1 : [프린터큐](https://www.acmicpc.net/problem/1966)
--- src/programmers/prob15/Solution.java
+++ src/programmers/prob15/Solution.java
@@ -1,61 +1,72 @@
 package programmers.prob15;
 
+
+import java.time.LocalTime;
 import java.util.Arrays;
 
 class Solution {
     // 셔틀 운행 횟수 n, 셔틀 운행 간격 t, 한 셔틀에 탈 수 있는 최대 크루 수 m, 크루가 대기열에 도착하는 시각을 모은 배열 timetable
     public String solution(int n, int t, int m, String[] timetable) {
-        // 셔틀은 09:00부터 총 n회 t분 간격으로 역에 도착하며, 하나의 셔틀에는 최대 m명의 승객이 탈 수 있다.
-        // 콘은 게으르기 때문에 같은 시각에 도착한 크루 중 대기열에서 제일 뒤에 선다.
-        // 또한, 모든 크루는 잠을 자야 하므로 23:59에 집에 돌아간다. 따라서 어떤 크루도 다음날 셔틀을 타는 일은 없다.
         String answer = ""; // 콘이 무사히 셔틀을 타고 사무실로 갈 수 있는 제일 늦은 도착 시각을 출력한다. 도착 시각은 HH:MM 형식이며, 00:00에서 23:59 사이의 값이 될 수 있다.
         Arrays.sort(timetable);
 
-        // 1. 18:01 ~ 23:59 분에 도착한 사람들은 다 집으로 돌려보낸다.
-        int crewCnt = 0;
-        for(String time : timetable) {
-            String[] hourMinute = time.split(":");
-            int hour = Integer.parseInt(hourMinute[0]);
-            int min = Integer.parseInt(hourMinute[1]);
-
-            if((hour==18 && min >=1) || hour>=19) {
-                crewCnt++;
-            }
-        }
-
-        // 버스 운행
-        int initBusHour = 9;
-        int initBusMin = 0;
-        for(int i=0; i<n; i++) {
+        // 마지막 버스 전까지 운행
+        int takeBusCnt = 0;
+        for(int i=0; i<n-1; i++) {
             int nextBusTerm = t*i;
-            int busArrivalHour = initBusHour + (nextBusTerm/60);
-            int busArrivalMin = initBusMin + (nextBusTerm%60);
-            int takeBusCnt = 0;
+            int busArrivalHour = 9 + (nextBusTerm/60);
+            int busArrivalMin = nextBusTerm%60;
+            LocalTime busArrivalTime = LocalTime.of(busArrivalHour, busArrivalMin);
+            int cnt = 0;
 
-            // 마지막 버스
-            if(i==n-1) {
-                // 2. 콘이 마지막 버스 타려고 할 때 남은 사람이 m-1 이하이면 콘은 마지막 버스 도착할 때 도착하면된다
-                // 3. 콘이 마지막 버스 차려고 할 때 남은 사람이 m명 이상이면 맨 뒤에서부터 m-1명 될 때 까지 뺀다
-            }
-
-            for(int j=crewCnt; j<timetable.length; j++) {
-                String[] crewArrivalInfo = timetable[j].split(":");
-                int crewArrivalHour = Integer.parseInt(crewArrivalInfo[0]);
-                int crewArrivalMin = Integer.parseInt(crewArrivalInfo[1]);
-
-                if(busArrivalHour<=crewArrivalHour && busArrivalMin<=crewArrivalMin && takeBusCnt<m) {
+            for(int j=takeBusCnt; j<timetable.length && cnt<m; j++) {
+                LocalTime crewArrivalTime = LocalTime.parse(timetable[j]);
+                if(crewArrivalTime.compareTo(busArrivalTime)<=0) {
                     takeBusCnt++;
-                    crewCnt++;
+                    cnt++;
                 }
             }
         }
 
+        // 18:01 ~ 23:59 분에 도착한 사람들은 다 집으로 돌려보낸다.
+        int goHomeIdx = timetable.length;
+        for(int i=takeBusCnt; i<timetable.length; i++) {
+            LocalTime arrivalTime = LocalTime.parse(timetable[i]);
+            if(arrivalTime.isAfter(LocalTime.parse("18:00")) && arrivalTime.isBefore(LocalTime.parse("00:00"))) {
+                goHomeIdx = i;
+                break;
+            }
+        }
 
+        int leftCrewCnt = goHomeIdx - takeBusCnt;
         int lastBusHour = (t*(n-1))/60+9;
         int lastBusMin = (t*(n-1))%60;
+        LocalTime lastBusTime = LocalTime.of(lastBusHour, lastBusMin);
 
+        if(leftCrewCnt < m) {
+            answer = lastBusTime.toString();
+        } else {
+            int targetIdx = takeBusCnt+m-1;
+            LocalTime targetTime = LocalTime.parse(timetable[targetIdx]);
+
+            if(targetTime.isAfter(lastBusTime)) {
+                answer = lastBusTime.toString();
+            } else {
+                while(targetIdx>takeBusCnt) {
+                    LocalTime target = LocalTime.parse(timetable[targetIdx]);
+                    LocalTime beforeTarget = LocalTime.parse(timetable[targetIdx-1]);
+
+                    if(beforeTarget.isBefore(target)) {
+                        break;
+                    }
+
+                    targetIdx--;
+                }
+                LocalTime ans = LocalTime.parse(timetable[targetIdx]).minusMinutes(1);
+                answer = ans.toString();
+            }
+        }
 
         return answer;
     }
-
 }
